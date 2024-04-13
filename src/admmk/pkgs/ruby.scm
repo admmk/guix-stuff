71c272900de06d7fd05f5a2e938388e4427c60c0
Adam <>
2024-04-14 01:44:42 +0300

    wip

--- src/admmk/pkgs/ruby.scm
+++ src/admmk/pkgs/ruby.scm
@@ -1,6 +1,7 @@
 (define-module (admmk pkgs ruby)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
+  #:use-module (gnu packages ruby)
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (guix build-system ruby))
